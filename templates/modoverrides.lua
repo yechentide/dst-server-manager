c1cd9a8341c1f0513d872324631fb7b4979f6089
yechentide <yechentide@gmail.com>
2021-11-27 20:35:35 +0900

    change the template of mods

--- templates/modoverrides.lua
+++ templates/modoverrides.lua
@@ -1,20 +1,2 @@
 return {
-    ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
-    ["workshop-1216718131"]={ configuration_options={ clean=true, lang=true, stack=true }, enabled=true },
-    ["workshop-2321974509"]={ configuration_options={ SIB=false, SSB=false }, enabled=true },
-    ["workshop-666155465"]={
-        configuration_options={
-        chestB=-1,
-        chestG=-1,
-        chestR=-1,
-        display_hp=-1,
-        food_estimation=-1,
-        food_order=0,
-        food_style=0,
-        lang="auto",
-        show_food_units=-1,
-        show_uses=-1 
-        },
-        enabled=true 
-    } 
 }
